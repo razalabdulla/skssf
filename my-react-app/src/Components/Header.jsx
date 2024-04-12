f3ce7a6e7b6116a0b72f683507f2670b1284aa1d
razal <razalabdulla285@gmail.com>
2024-04-12 15:26:42 +0530

    2nd

--- my-react-app/src/Components/Header.jsx
+++ my-react-app/src/Components/Header.jsx
@@ -1,77 +1,66 @@
-import React,{useState} from 'react'
-
-import {
-    MDBContainer,
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarToggler,
-    MDBIcon,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBBtn,
-    MDBDropdown,
-    MDBDropdownToggle,
-    MDBDropdownMenu,
-    MDBDropdownItem,
-    MDBCollapse,
-  } from 'mdb-react-ui-kit';
-  
+import React, { useState, useEffect } from 'react';
+import './Header.css'
 function Header() {
-    const [openBasic, setOpenBasic] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false); // State to manage the open/close state of the navbar
+
+    // Function to toggle the navbar
+    const toggleNavbar = () => {
+        setMenuOpen(!menuOpen); // Toggle the state
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > 0) {
+                navbar.classList.add('navbar-scroll');
+                // Change color of all <a> tags to black
+                const aTags = document.querySelectorAll('.navbar a');
+                aTags.forEach(aTag => {
+                    aTag.style.color = 'black';
+                });
+            } else {
+                navbar.classList.remove('navbar-scroll');
+                // Reset color of all <a> tags
+                const aTags = document.querySelectorAll('.navbar a');
+                aTags.forEach(aTag => {
+                    aTag.style.color = ''; // Reset to default
+                });
+            }
+        });
+
+        // Clean up event listener
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []); // Empty dependency array ensures the effect runs only once
 
     return (
         <>
-     <MDBNavbar expand='lg' light bgColor='light'>
-  <MDBContainer fluid>
-    <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
+            <header></header>
+            <nav className="navbar navbar-expand-sm fixed-top navbar-light">
+                <div className="container">
+                    <a className="navbar-brand main-head" href="#" style={{ fontSize: '42px', color: 'black', display: 'flex',fontWeight:'500',marginTop:'9px', flexDirection: 'column' ,fontFamily:'inherit'}}>
+                        SKSSF <span className="sub-head" >pilavalapp unit</span>
+                    </a>
+                    <button className="navbar-toggler" type="button" onClick={toggleNavbar} aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbar1">
+                        <ul className="navbar-nav nvr">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Poster</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-    <MDBNavbarToggler
-      aria-controls='navbarSupportedContent'
-      aria-expanded='false'
-      aria-label='Toggle navigation'
-      onClick={() => setOpenBasic(!openBasic)}
-    >
-      <MDBIcon icon='bars' fas />
-    </MDBNavbarToggler>
-
-    <MDBCollapse navbar open={openBasic}>
-      <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-        <MDBNavbarItem>
-          <MDBNavbarLink active aria-current='page' href='#'>
-            Home
-          </MDBNavbarLink>
-        </MDBNavbarItem>
-        <MDBNavbarItem>
-          <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-        </MDBNavbarItem>
-
-        <MDBNavbarItem>
-          <MDBDropdown>
-            <MDBDropdownToggle tag='a' className='nav-link' role='button'>
-              Dropdown
-            </MDBDropdownToggle>
-            <MDBDropdownMenu>
-              <MDBDropdownItem link>Action</MDBDropdownItem>
-              <MDBDropdownItem link>Another action</MDBDropdownItem>
-              <MDBDropdownItem link>Something else here</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
-        </MDBNavbarItem>
-
-        <MDBNavbarItem>
-          <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-            Disabled
-          </MDBNavbarLink>
-        </MDBNavbarItem>
-      </MDBNavbarNav>
-
-    </MDBCollapse>
-  </MDBContainer>
-</MDBNavbar>
-
-      </>
-  )
+            <div className="sticky-top bg-white hidden-spacer"> </div>
+        </>
+    );
 }
 
-export default Header
+export default Header;
